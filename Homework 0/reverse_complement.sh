44f41aa2aa717fe90bda0dfa5ca97f055ea1d993
Jacob Lorenz <58476572+TheExplorativeBadger@users.noreply.github.com>
2021-09-08 00:39:04 -0500

    Adding gitignore

---
+++ Homework 0/reverse_complement.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# Uncomment the appropriate line below depending on your programming
+# language of choice.  To uncomment a line remove the '#' character
+# from the beginning of the line.
+
+# Uncomment the line below if you write a Java program
+java reverse_complement $1
+
+# Uncomment the line below for a Python program
+#python reverse_complement.py $1
+
+# Uncomment the line below for a Perl program
+#perl reverse_complement.pl $1
+
+# Uncomment the line below to run a compiled C or C++ program
+#./reverse_complement $1
+
+# Uncomment the line below for an R program
+#Rscript reverse_complement.R $1
